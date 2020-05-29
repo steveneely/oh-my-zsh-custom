4c1e6e247cfe7cda7616250c3591ffeee99ec721
Steve Neely <sneely@Steves-2019-MBP.hsd1.co.comcast.net>
2020-05-28 22:11:36 -0600

    Changing to use JDK 14 only

--- plugins/java/java.plugin.zsh
+++ plugins/java/java.plugin.zsh
@@ -1,4 +1,2 @@
-export JAVA6_HOME=$(/usr/libexec/java_home -v 1.6)
-export JAVA7_HOME=$(/usr/libexec/java_home -v 1.7)
-export JAVA8_HOME=$(/usr/libexec/java_home -v 1.8)
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)
+export JAVA14_HOME=$(/usr/libexec/java_home -v 14)
+export JAVA_HOME=$(/usr/libexec/java_home -v 14)

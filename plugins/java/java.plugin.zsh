63194a81d8819e568643707fb8b3f7b0fd410299
Steve Neely <steve.neely@gmail.com>
2020-10-20 12:41:32 -0600

    bumping Java version to jdk 15

--- plugins/java/java.plugin.zsh
+++ plugins/java/java.plugin.zsh
@@ -1,2 +1,2 @@
-export JAVA14_HOME=$(/usr/libexec/java_home -v 14)
-export JAVA_HOME=$(/usr/libexec/java_home -v 14)
+export JAVA14_HOME=$(/usr/libexec/java_home -v 15)
+export JAVA_HOME=$(/usr/libexec/java_home -v 15)
